82f602f5ad9e14670f99656966d3c60c33f60d38
Marcel <halanocca21@gmail.com>
2020-05-21 16:42:20 -0500

    Tipos de maps implementado con Widget Speed Dial

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Maps extends StatefulWidget {
@@ -10,6 +11,7 @@ class Maps extends StatefulWidget {
 class _MapsState extends State<Maps> {
 
   LatLng posicion = LatLng(-16.398584,-71.536896);
+  MapType  mapType = MapType.normal;
 
   //-16.398584, -71.536896
   @override
@@ -24,12 +26,55 @@ class _MapsState extends State<Maps> {
           GoogleMap(
             initialCameraPosition: CameraPosition(
               target:posicion,
-              zoom: 20 )
-          )
+              zoom: 13       
+            ),
+             mapType:mapType,
+          ),
+           
+         _speedDial(),
 
         ],
       ),
       
+    );
+  }
+
+  Widget _speedDial(){
+
+    return Container(
+      padding: EdgeInsets.only(bottom: 10.0,right: 10.0),
+      child: SpeedDial(
+            animatedIcon: AnimatedIcons.menu_close,
+            overlayColor: Colors.black,
+            overlayOpacity: 0.5,
+            elevation: 8.0,
+            children: [
+              SpeedDialChild(
+                label: "NORMAL",
+                child: Icon(Icons.room),
+                onTap: ()=>setState(()=>mapType= MapType.normal)
+              ),
+
+              SpeedDialChild(
+                label: "SATELLITE",
+                child: Icon(Icons.satellite),
+                onTap: ()=>setState(()=>mapType= MapType.satellite)
+              ),
+
+              SpeedDialChild(
+                label: "HYBRID",
+                child: Icon(Icons.compare),
+                onTap: ()=>setState(()=>mapType= MapType.hybrid)
+              ),
+
+              SpeedDialChild(
+                label: "TERRAIN",
+                child: Icon(Icons.terrain),
+                onTap: ()=>setState(()=>mapType= MapType.terrain)
+              )
+
+            ],
+          ),
     );
   }
 }
